23410a63c901859eee17021c34ea692748376e9c
Aleksey Belchenkov <u608110@gmail.com>
2021-07-09 17:57:38 +0400

    Validate register form

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,8 +1,35 @@
-import { Form as VeeForm, Field as VeeField } from 'vee-validate';
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+} from 'vee-validate';
+import {
+  required,
+  min,
+  max,
+  min_value as minVal,
+  max_value as maxVal,
+  alpha_spaces as alphaSpaces,
+  email,
+  confirmed,
+  not_one_of as excluded,
+} from '@vee-validate/rules';
 
 export default {
   install(app) {
     app.component('VeeForm', VeeForm);
     app.component('VeeField', VeeField);
+    app.component('ErrorMessage', ErrorMessage);
+
+    defineRule('required', required);
+    defineRule('min', min);
+    defineRule('max', max);
+    defineRule('email', email);
+    defineRule('min_value', minVal);
+    defineRule('max_value', maxVal);
+    defineRule('alpha_spaces', alphaSpaces);
+    defineRule('confirmed', confirmed);
+    defineRule('excluded', excluded);
   },
 };
