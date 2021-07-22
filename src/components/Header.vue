8d8778c6759ab2c148a5b9421411f541b767ed1c
Aleksey Belchenkov <u608110@gmail.com>
2021-07-22 15:05:26 +0400

    Settings Router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,18 +2,21 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        to="/"
+      >Music</router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li v-if="!userLoggedIn">
-            <a
+            <router-link
               class="px-2 text-white"
-              href="#"
+              to="/login"
               @click.prevent="toggleAuthModal"
-            >Login / Register</a>
+            >Login / Register</router-link>
           </li>
           <template v-else>
             <li>
