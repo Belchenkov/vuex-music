a0e755a5929726887881babfc1320c6da4841369
Aleksey Belchenkov <u608110@gmail.com>
2021-08-12 13:09:58 +0400

    Get songs by scrolling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,20 +52,65 @@ export default {
   data() {
     return {
       songs: [],
+      maxPerPage: 3,
+      pendingRequest: false,
     };
   },
   components: {
     AppSongItem,
   },
   async created() {
-    const snapshots = await songsCollection.get();
+    await this.getSongs();
 
-    snapshots.forEach((document) => {
-      this.songs.push({
-        docID: document.id,
-        ...document.data(),
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    // eslint-disable-next-line consistent-return
+    async getSongs() {
+      if (this.pendingRequest) {
+        return false;
+      }
+      this.pendingRequest = true;
+
+      let snapshots = [];
+
+      if (this.songs.length) {
+        const lastDoc = await songsCollection
+          .doc(this.songs[this.songs.length - 1].docID)
+          .get();
+
+        snapshots = await songsCollection
+          .orderBy('modifiedName')
+          .startAfter(lastDoc)
+          .limit(this.maxPerPage)
+          .get();
+      } else {
+        snapshots = await songsCollection
+          .orderBy('modifiedName')
+          .limit(this.maxPerPage)
+          .get();
+      }
+
+      snapshots.forEach((document) => {
+        this.songs.push({
+          docID: document.id,
+          ...document.data(),
+        });
       });
-    });
+      this.pendingRequest = false;
+    },
+    handleScroll() {
+      const { scrollTop, offsetHeight } = document.documentElement;
+      const { innerHeight } = window;
+      const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+      if (bottomOfWindow) {
+        this.getSongs();
+      }
+    },
   },
 };
 </script>
