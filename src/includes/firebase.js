613d99b21738b1652e81795a6a8854665f051b41
Алексей Бельченков <u608110@gmail.com>
2022-01-19 11:56:03 +0400

    Firebase Data Persistence

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -18,6 +18,11 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 
+db.enablePersistence()
+  .catch((error) => {
+    console.error(`Firebase persistence error ${error.code}`);
+  });
+
 const usersCollection = db.collection('users');
 const songsCollection = db.collection('songs');
 const commentsCollection = db.collection('comments');
