4c1ce432d9f4864cc19086ffee1c5e85ee80dbd5
Алексей Бельченков <u608110@gmail.com>
2022-01-26 11:37:28 +0400

    Modules store

---
+++ src/store/modules/auth.js
@@ -0,0 +1,60 @@
+import { auth, usersCollection } from '../../includes/firebase';
+
+export default {
+  state: {
+    authModalShow: false,
+    userLoggedIn: false,
+  },
+  getters: {
+    authModalShow: (state) => state.authModalShow,
+  },
+  mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    },
+    toggleAuth: (state) => {
+      state.userLoggedIn = !state.userLoggedIn;
+    },
+  },
+  actions: {
+    async register({ commit }, {
+      name,
+      password,
+      email,
+      age,
+      country,
+    }) {
+      const userCred = await auth.createUserWithEmailAndPassword(email, password);
+
+      await usersCollection
+        .doc(userCred.user.uid)
+        .set({
+          name,
+          email,
+          age,
+          country,
+        });
+
+      await userCred.user.updateProfile({
+        displayName: name,
+      });
+
+      commit('toggleAuth');
+    },
+    async login({ commit }, { email, password }) {
+      await auth.signInWithEmailAndPassword(email, password);
+      commit('toggleAuth');
+    },
+    initLogin({ commit }) {
+      const user = auth.currentUser;
+
+      if (user) {
+        commit('toggleAuth');
+      }
+    },
+    async signOut({ commit }) {
+      await auth.signOut();
+      commit('toggleAuth');
+    },
+  },
+};
